754468fc0ecd0c76bfbfb33ebfcf88e804c5c462
Margal <mgalloperalta@gmail.com>
2018-12-15 16:27:46 +0100

    updated readme

--- Readme_M1.sh
+++ Readme_M1.sh
@@ -1,5 +1,5 @@
 # Project's name
-Angry Biker
+Angry Fietser
 
 ## Description
 The player is an urban cyclist that hates turists. The ciclist must crash as many turists as posible
@@ -10,14 +10,12 @@ to win points. Avoid crashing against cars.
 ## MVP (DOM - CANVAS)
 MVP definition, deliverables.
 
-- 2D canvas context with a road and sidewalk background the move.
+- 2D canvas context with a road and sidewalk background that move.
 - Bike is static at the left end of the screen but the player is able to move the bike up and down, giving
   the impression the bike changes lanes on the road. 
 - Random turists appear on the road.
 - Collision between turist and bike give points to the player.
 
-
-## Backlog
 1. Draw road.
 2. Draw static biker element.
 3. Add movement effect to road.
@@ -26,7 +24,7 @@ MVP definition, deliverables.
 6. Generate collision events between player and turists. Turists elements dissapear on collission. 
 7. Biker gains points on collissions with turists. Show points on DOM. 
 
-EXTRAS
+## Backlog
 
 8. Set an interval that makes the background move faster every X amount of time.
 9. Add cars (obstacles) the biker needs to avoid. 
@@ -36,16 +34,71 @@ EXTRAS
 12. Improve graphics
 
 ## Data structure
-Classes and methods definition.
+
+- Class Game
+    - Properties:
+        - biker
+        - tourists
+        - context
+        - rows
+        - columns
+        - bikerPoints
+        - bikerHealth
+        - road / background
+    - Functions:
+        - _draw = board/biker/tourists/road
+        - update : all of the above
+        - start
+        - Pause
+
+- Class Biker
+    - Properties:
+        - position
+        - health
+    - Functions:
+        - moveRight
+        - moveLeft
+        - collision: with tourists
+
+- Class tourists
+    - Properties:
+        - position
+    - Functions:
+        - collision: with biker
+
+- Class background & road ?
+    - Properties
+        - speed
+    - Functions
+        - Speed
+
+
+
+EXTRA - Backlog
+
+- Class Cars
+    - Properties:
+        - position
+    - Functions:
+        - collision: with biker
 
 
 ## States y States Transitions
 Definition of the different states and their transition (transition functions)
 
-- splashScreen
+- initial screen
+    - playing instructions
+    - start button
+
 - gameScreen
+    - playing board (canvas)
+    - points and lives (DOM)
+    - Pause button (DOM)
+
 - gameoverScreen
-- winScreen
+    - Bye Bye message
+    - Restart button
+
 
 
 ## Task
@@ -60,8 +113,8 @@ https://trello.com/b/mLOB9V0M/angry-fietser
 
 
 ### Git
-URls for the project repo and deploy
-[Link Repo](http://github.com)
+https://github.com/MarcGal/Angry-Fietser
+
 [Link Deploy](http://github.com)
 
 
