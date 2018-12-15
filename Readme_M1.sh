08da56c8aa0aa93310f47d2a603116d9bf86b520
Margal <mgalloperalta@gmail.com>
2018-12-15 15:04:17 +0100

    initial commit.

---
+++ Readme_M1.sh
@@ -0,0 +1,69 @@
+# Project's name
+Angry Biker
+
+## Description
+The player is an urban cyclist that hates turists. The ciclist must crash as many turists as posible
+to win points. Avoid crashing against cars.
+
+
+
+## MVP (DOM - CANVAS)
+MVP definition, deliverables.
+
+- 2D canvas context with a road and sidewalk background the move.
+- Bike is static at the left end of the screen but the player is able to move the bike up and down, giving
+  the impression the bike changes lanes on the road. 
+- Random turists appear on the road.
+- Collision between turist and bike give points to the player.
+
+
+## Backlog
+1. Draw road.
+2. Draw static biker element.
+3. Add movement effect to road.
+4. Add up/down movement to biker.
+5. Generate random turists.
+6. Generate collision events between player and turists. Turists elements dissapear on collission. 
+7. Biker gains points on collissions with turists. Show points on DOM. 
+
+EXTRAS
+
+8. Set an interval that makes the background move faster every X amount of time.
+9. Add cars (obstacles) the biker needs to avoid. 
+10. Avoid tourists and cars being generated on the same position.
+10. Generate collission between car and biker. Upon collission the biker looses health. 
+11. Add forward and backward movement functionality to the biker.
+12. Improve graphics
+
+## Data structure
+Classes and methods definition.
+
+
+## States y States Transitions
+Definition of the different states and their transition (transition functions)
+
+- splashScreen
+- gameScreen
+- gameoverScreen
+- winScreen
+
+
+## Task
+https://trello.com/b/mLOB9V0M/angry-fietser
+
+
+## Links
+
+
+### Trello
+https://trello.com/b/mLOB9V0M/angry-fietser
+
+
+### Git
+URls for the project repo and deploy
+[Link Repo](http://github.com)
+[Link Deploy](http://github.com)
+
+
+### Slides
+https://slides.com/margal/angry-fietser/edit
