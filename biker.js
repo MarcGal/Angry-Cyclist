f7b642ab440416c5298c2ce76f4632095ac0fec8
Margal <mgalloperalta@gmail.com>
2018-12-18 19:47:10 +0100

    add biker and game classes to corresponding js files. Biker with property position. game with properties biker, tourist, points, lives, ctx

---
+++ biker.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+class Biker {
+
+  constructor (position){
+    this.position = position;
+  }
+
+}
---
+++ game.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+
+class Game {
+
+  constructor (){
+    this.biker = biker;
+    this.tourist = tourist;
+    this.ctx = ctx;
+    this.points = 0;
+    this.lives = 3;
+  }
+}
