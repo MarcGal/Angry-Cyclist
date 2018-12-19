1398f51dbb6faedc302b1f100f3920dc0e886eae
Margal <mgalloperalta@gmail.com>
2018-12-19 18:08:12 +0100

    Add speed property to the Tourist class constructor. Add movement function to the tourist class

--- tourist.js
+++ tourist.js
@@ -1,6 +1,14 @@
 /*jshint esversion: 6 */
 class Tourist {
-  constructor (position){
-    this.position = position;
+  constructor (){
+    this.witdh = 15;
+    this.height = 15;
+    this.positionX = 820;
+    this.positionY = 350;
+    this.speed = -6;
+  }
+
+  move (){
+    this.positionX -= this.speed;
   }
 }
