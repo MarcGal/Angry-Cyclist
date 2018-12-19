f43e95dba3f08aa65ddfb91317824ac1edc8c8e6
Margal <mgalloperalta@gmail.com>
2018-12-19 16:21:22 +0100

    Add properties: biker, tourist, ctx, points, lives, and interval game. Add draw board and draw biker functions. Add update function that calls draw biker and board, and calls it self with request animation frame to create the infinfite loop

--- game.js
+++ game.js
@@ -2,11 +2,35 @@
 
 class Game {
 
-  constructor (){
-    this.biker = biker;
-    this.tourist = tourist;
-    this.ctx = ctx;
+  constructor (options){
+    this.biker = new Biker();
+    this.tourist = new Tourist();
+    this.ctx = options.ctx;
     this.points = 0;
     this.lives = 3;
+    this.intervalGame = undefined;
   }
+
+  _drawBoard () { 
+    this.ctx.fillStyle = 'green';
+    this.ctx.fillRect(0,0, 800, 400);
+  }
+
+  _drawBiker (){
+    this.ctx.fillStyle = 'blue';
+    this.ctx.fillRect(this.biker.positionX, this.biker.positionY, this.biker.witdh, this.biker.height);
+  }
+
+  
+  start (updatePoints){
+    this.points = updatePoints;
+    this._update();
+  }
+
+  _update (){
+    this._drawBoard();
+    this._drawBiker();
+    this.intervalGame = window.requestAnimationFrame(this._update.bind(this));
+  }
+
 }
