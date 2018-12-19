1bc2d73249ae6418c9cfd6dd117755b7460fc48b
Margal <mgalloperalta@gmail.com>
2018-12-19 16:27:39 +0100

    Add properties to biker class constructor width, height, x and y

--- biker.js
+++ biker.js
@@ -1,8 +1,10 @@
 /*jshint esversion: 6 */
 class Biker {
 
-  constructor (position){
-    this.position = position;
+  constructor (){
+    this.witdh = 15;
+    this.height = 15;
+    this.positionX = 25;
+    this.positionY = 350;
   }
-
 }
