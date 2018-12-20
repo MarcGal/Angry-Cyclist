4423a4f15ee1892ca80a9c4f312e2b6df150b664
Margal <mgalloperalta@gmail.com>
2018-12-20 17:36:28 +0100

    Add start div to html, including start button functionality that displays the game element

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ document.onload = function() {
   const start = document.querySelector('#start-page');
   const play = document.querySelector('#play');
   
-  start.onclick = function (){
-    play.style = 'display: block';
-    console.log(start);
-    start.style = 'display: none';
-  };
+  // start.onclick = function (){
+  //   play.style = 'display: block';
+  //   start.style = 'display: none';
+  // };
 
   const canvas = document.getElementById('game');
   const ctx = canvas.getContext('2d');
