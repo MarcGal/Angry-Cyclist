6ed625ea6e1027ea281eb631dd5645ad6f512546
Margal <mgalloperalta@gmail.com>
2018-12-27 17:09:21 +0100

    Assign restart button to variable, assign onclick event to button that calls the location.reload method

--- main.js
+++ main.js
@@ -2,11 +2,17 @@
 document.onload = function() {
   const start = document.querySelector('#start-page');
   const play = document.querySelector('#play');
+  const restart = document.querySelector('#re-start');
   
   // start.onclick = function (){
   //   play.style = 'display: block';
   //   start.style = 'display: none';
   // };
+
+
+  restart.onclick = function (){
+    document.location.reload();
+  };
 
   const canvas = document.getElementById('game');
   const ctx = canvas.getContext('2d');
