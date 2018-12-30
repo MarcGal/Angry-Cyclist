6fdce9b18eb1682d586b1711ae79fadfc0cbeb86
Margal <mgalloperalta@gmail.com>
2018-12-30 15:55:21 +0100

    Create background js file..contains class background with image, width and speed properties

---
+++ background.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+
+class Background {
+
+  constructor (){
+    this.img = new Image (800, 400);
+    this.img.src = 'img/AMS_img.png';
+    this.x = 0;
+    this.width = 800;
+    this.speed = -0.5;
+  }
+}
