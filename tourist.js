12daa83d073a2848dd2f210aa5388534ba3b8ca8
Margal <mgalloperalta@gmail.com>
2019-01-03 18:11:40 +0100

    Add this.img property containing the new image method. Add all necessarry properties for a functioning sprite

--- tourist.js
+++ tourist.js
@@ -1,10 +1,24 @@
 /*jshint esversion: 6 */
 class Tourist {
   constructor (){
-    this.witdh = 15;
-    this.height = 15;
+    this.img = new Image ();
+    this.img.src = 'img/drunk_english.png';
+    this.width = 128;
+    this.height = 192;
+    // this.imgWitdh = 15; old properties
+    // this.imgHeight = 25; old properties
+    this.imgRows = 4;
+    this.imgCols = 4;
+    this.rowLeft = 1;
+    this.rowDown = 0;
+    this.widthFrame = this.width/this.imgCols;
+    this.heightFrame = this.height/this.imgRows;
+    this.srcX = 32;
+    this.srcY = 48;
+    this.currentFrame = 0;
+    this.frameCount = 4;
     this.positionX = 810;
-    this.positionY = 230;
+    this.positionY = 200;
     this.speed = 0.5;
     this.crossingPoint = this.crossingPoint();
   }
