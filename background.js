87e2e57f256ed45e8a9b7a3f7b3091e235386140
Margal <mgalloperalta@gmail.com>
2019-01-25 16:46:43 +0100

    change background, remove line

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ class Background {
 
   constructor (){
     this.img = new Image (1000, 500);
-    this.img.src = 'img/amsterdam3.jpg';
+    this.img.src = 'img/ams4.jpg';
     this.x = 0;
     this.width = 1000;
     this.speed = -0.2;
